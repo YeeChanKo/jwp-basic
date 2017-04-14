4a549a4ad7865d030c04d60d114fc875cc3cb25f
viz <yeechan.ko@nhnnext.org>
2017-04-14 21:08:11 +0900

    작업작업

---
+++ src/main/java/next/controller/qna/GetQuestionsController.java
@@ -0,0 +1,17 @@
+package next.controller.qna;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.mvc.Controller;
+
+public class GetQuestionsController implements Controller {
+
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
